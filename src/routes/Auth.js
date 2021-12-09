549d4fba8391faf5bf9c20105f03406838405b87
taekwon_yoo <takun617@gmail.com>
2021-12-09 18:40:02 +0900

    로그인 구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,19 +1,44 @@
+import { authService, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'fbase';
 import React, { useState } from 'react';
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
-    const {target: {name, value}} = event;
-    if(name === "email") {
+    const { target: { name, value } } = event;
+    if (name === "email") {
       setEmail(value);
-    } else if(name ==="password"){
+    } else if (name === "password") {
       setPassword(value);
     }
   }
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create account
+        data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password);
+      }
+      else {
+        // log in
+        data = await signInWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
   }
+  const toggleAccount = () => setNewAccount((prev) => !prev);
   return (<div>
     <form onSubmit={onSubmit}>
       <input
@@ -32,8 +57,10 @@ const Auth = () => {
         value={password}
         onChange={onChange}
       />
-      <input type="submit" value="Log In" />
+      <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+      {error}
     </form>
+    <span onClick={toggleAccount}>{newAccount ? "Sign in." : "Create Account"}</span>
     <div>
       <button>Continue with Google</button>
       <button>Continue with Github</button>
