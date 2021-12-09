8abf7ea9f34e965950aeaae687aa2a77b0015c5b
taekwon_yoo <takun617@gmail.com>
2021-12-09 17:55:46 +0900

    firebase 적용, import 절대경로 적용, 기본 로그인 폼 구현

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+
+import AppRouter from "components/Router";
+import React, { useState } from "react";
+import { authService } from "fbase";
+
+function App() {
+  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn = {isLoggedIn}/>
+      <footer>&copy; Nwitter {new Date().getFullYear()} </footer>
+    </>
+  );
+}
+
+export default App;
